6721245fbbce7b75eae6c64c913e94434e74fff0
Ilya Danilenko <47718218+IlyaDanilenko@users.noreply.github.com>
2025-09-17 22:33:28 +0300

    Init config impl

---
+++ src/core/config.rs
@@ -0,0 +1,30 @@
+use std::{collections::HashMap};
+
+// structs define
+
+pub struct ConfigLoader {
+    configs: Option<HashMap<String, String>>,
+}
+
+// Impl block
+
+impl ConfigLoader {
+    pub fn new() -> ConfigLoader {
+        ConfigLoader { configs: None }
+    }
+
+    pub fn get(&self, names: Vec<String>) -> HashMap<String, String> {
+        match &self.configs {
+            Some(configs) => {
+                let mut result = HashMap::new();
+                for name in names {
+                    if let Some(value) = configs.get(&name) {
+                        result.insert(name, value.clone());
+                    }
+                }
+                result
+            },
+            None => HashMap::new(),
+        }
+    }
+}
