13dae31a1ae573a0d14711ee2d74b51f02810948
Maxim Yamkin <makson.yamkin@mail.ru>
2025-09-29 20:06:30 +0300

    add similarity search and vectorizer

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,20 @@
 use std::collections::HashMap;
 
 use crate::core::objects::Metadata;
+use crate::vectors::embeddings::make_embeddings;
 
 pub mod core;
+pub mod vectors;
+
 
 fn main() {
     let mut new_data = HashMap::new();
     new_data.insert("name".to_string(), "kek".to_string());
-    let meta = Metadata::new(new_data);
+    let meta = Metadata::new(Some(new_data));
     println!("{}", meta.to_string());
+
+    match make_embeddings("English") {
+        Ok(embeddings) => {println!("Embedding dimension: {}", embeddings.len())}
+        Err(_e) => {}
+    }
 }
